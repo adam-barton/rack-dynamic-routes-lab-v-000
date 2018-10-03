223f77653dd9c4f5d9f3f1f701cf1db802bef8c9
Adam Barton <adam.barton333@gmail.com>
2018-10-03 20:23:57 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,15 +4,16 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
+    if req.path.match(/items/)
+      item_name = req.path.split(/item/).last
+      item = @@items.find{|i| i.price}
+    end
+
     if req.path != "/items"
       resp.write "Route not found"
       resp.status = 404
     end
 
-    # if requ.path.match(/items/)
-    #   item_name = req.path.split(/item/).last
-    #   item =
-    # end
     resp.finish
   end
 end
